de3424af0ffc2c567074867c3289c67136e7f2df
aguywhosaguy <caseypeg8@gmail.com>
2026-04-12 19:39:57 -0500

    feat: added home manager

--- flake.nix
+++ flake.nix
@@ -3,13 +3,29 @@
 
   inputs = {
     nixpks.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    home-manager = {
+      url = "github:nix-comunity/home-manager/release-25.11";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
   };
 
-  outputs = { self, nixpkgs, ...}@inputs: {
-    nixosConfigurations.nimonix = nixpkgs.lib.nixosSystem {
-      modules = [
-        ./configuration.nix
-      ];
+  outputs = { self, nixpkgs, home-namager, ...}@inputs: {
+    nixosConfigurations = {
+      nimonix = nixpkgs.lib.nixosSystem {
+        modules = [ 
+          ./configuration.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+
+	    home-manager.users.henryw = import ./home.nix;
+	  };
+        ];
+      };
     };
   };
 }
